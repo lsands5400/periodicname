c049313d8b70ab2e96393185fb352485c809d4bc
lsands5400 <lsands@g.hmc.edu>
2025-10-15 14:07:21 -0700

    Wrote main with commented out lines that use unfinished functions in periodicname.hpp.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
+/**
+ * \file main.cpp
+ * \author Lindsey Sands
+ *
+ * \brief Main function for PeriodicName converter
+ *
+ */
+
 #include "periodicname.hpp"
 #include <string>
 #include <iostream>
 
 void main() {
-    std::cout << "Enter the message to convert"
+    std::cout << "Enter the message to convert";
     std::string input;
-    std::cin >> input;   
+    std::cin >> input; 
+    // PeriodicName userOutput = PeriodicName(input);
+    // std::string product = userOutput.strToSymb;
+    // std::cout << product;
 }
