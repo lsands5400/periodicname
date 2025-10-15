5ebe4a59fa06bd6e7b0467fe54c8484ef5911e72
lsands5400 <lsands@g.hmc.edu>
2025-10-15 13:56:22 -0700

    Added main.cpp file with cin and cout

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include "periodicname.hpp"
+#include <string>
+#include <iostream>
+
+void main() {
+    std::cout << "Enter the message to convert"
+    std::string input;
+    std::cin >> input;   
+}
