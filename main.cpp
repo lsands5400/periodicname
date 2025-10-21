53e444dddfc7e089fb4780d18d92a9cc186d6b2d
lsands5400 <lsands@g.hmc.edu>
2025-10-21 16:36:14 -0700

    Added elements test to main.cpp

--- main.cpp
+++ main.cpp
@@ -7,13 +7,20 @@
  */
 
 #include "periodicname.hpp"
+#include "elements.hpp"
 #include <string>
 #include <iostream>
 
 void main() {
-    std::cout << "Enter the message to convert";
-    std::string input;
-    std::cin >> input; 
+    // Testing elements class
+    Elements elements;
+    auto output = elements.getSingleCharList();
+    for (int i;  i = 0; i++) {
+        std::cout << output[i];
+    }
+    // std::cout << "Enter the message to convert";
+    // std::string input;
+    // std::cin >> input; 
     // PeriodicName userOutput = PeriodicName(input);
     // std::string product = userOutput.strToSymb;
     // std::cout << product;
